8a22bb405153f389075fbf232ac99653b54a0c6b
Toni Perkovic <toperkov@gmail.com>
2017-11-07 13:17:14 +0100

    Update hash_name.py

--- Lab2/Code/hash_name.py
+++ Lab2/Code/hash_name.py
@@ -1,32 +1,35 @@
-from cryptography.hazmat.primitives.ciphers import (
-    Cipher,
-    algorithms,
-    modes
-)
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding, hashes
-import os
+from cryptography.hazmat.primitives import hashes
 import itertools
 import time
 import random
 import base64
 
-def calculateHash (student_name, randomNumber):
-	toBeHashed = student_name + randomNumber
-	digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-	digest.update(toBeHashed)
-	hash = digest.finalize()
-	return base64.b64encode(hash)
+PASS_SIZE = 7
+PREZIME_IME = 'Perkovic Toni'
+
+def calculateHash(student_name, randomNumber):
+    toBeHashed = student_name + randomNumber
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+
+    if not isinstance(toBeHashed, bytes):
+        toBeHashed = toBeHashed.encode()
+
+    digest.update(toBeHashed)
+    hash = digest.finalize()
+    return base64.b64encode(hash)
+
 
 if __name__ == '__main__':
 
-	pass_size = 7
-	randomNumber = ''.join(str(x) for x in list(map(random.randrange, [10]*pass_size)))
-	hash = calculateHash('Zovko Luka', randomNumber)
+    randomNumber = ''.join(str(x) for x in list(
+        map(random.randrange, [10]*PASS_SIZE))
+    )
+    hash = calculateHash(PREZIME_IME, randomNumber)
 
-	f_out = open("secret.enc", 'wb')
-	f_out.write(hash)
-	f_out.close()
+    f_out = open("hashvalue", 'wb')
+    f_out.write(hash)
+    f_out.close()
 
-	print(hash)
-	print(randomNumber)
+    print(hash)
+    print(randomNumber)
